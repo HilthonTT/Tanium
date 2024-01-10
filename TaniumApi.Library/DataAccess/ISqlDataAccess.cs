0535c63abd659f410cbf0342d1b05383aaecf11c
Timothy Tandt <Timothy.tandt@outlook.be>
2024-01-10 16:12:14 +0100

    [Implementation] Added SqlDataAccess

---
+++ TaniumApi.Library/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+
+namespace TaniumApi.Library.DataAccess;
+public interface ISqlDataAccess
+{
+    void CommitTransaction();
+    Task<List<T>> GetAllDataAsync<T>(string storedProcedure, DynamicParameters parameters, string connectionStringName = "Default");
+    Task<T> GetDataAsync<T>(string storedProcedure, DynamicParameters parameters = null, string connectionStringName = "Default");
+    Task<List<T>> LoadAllDataInTransactionAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    Task<T> LoadDataInTransactionAsync<T>(string storedProcedure, DynamicParameters parameters = null);
+    void RollbackTransaction();
+    Task<int> SaveDataAsync(string storedProcedure, DynamicParameters parameters = null, string connectionStringName = "Default");
+    void StartTransaction(string connectionStringName = "Default");
+}
---
+++ TaniumApi.Library/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,117 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace TaniumApi.Library.DataAccess;
+public class SqlDataAccess(IConfiguration config) : ISqlDataAccess
+{
+    private const string DefaultDatabase = "Default";
+    private readonly IConfiguration _config = config;
+    private static IDbConnection _connection;
+    private static IDbTransaction _transaction;
+
+    private string GetConnectionString(string connectionStringName)
+    {
+        return _config.GetConnectionString(connectionStringName);
+    }
+
+    private static void Dispose()
+    {
+        _connection?.Dispose();
+        _transaction?.Dispose();
+
+        _transaction = null;
+        _connection = null;
+    }
+
+    public async Task<List<T>> GetAllDataAsync<T>(
+        string storedProcedure,
+        DynamicParameters parameters,
+        string connectionStringName = DefaultDatabase)
+    {
+        string connectionString = GetConnectionString(connectionStringName);
+
+        using var connection = new SqlConnection(connectionString);
+        var rows = await connection.QueryAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return rows.ToList();
+    }
+
+    public async Task<T> GetDataAsync<T>(
+        string storedProcedure,
+        DynamicParameters parameters = null,
+        string connectionStringName = DefaultDatabase)
+    {
+        string connectionString = GetConnectionString(connectionStringName);
+
+        using var connection = new SqlConnection(connectionString);
+        var row = await connection.QueryFirstOrDefaultAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return row;
+    }
+
+    public async Task<int> SaveDataAsync(
+        string storedProcedure,
+        DynamicParameters parameters = null,
+        string connectionStringName = DefaultDatabase)
+    {
+        string connectionString = GetConnectionString(connectionStringName);
+
+        using var connection = new SqlConnection(connectionString);
+        int result = await connection.ExecuteAsync(storedProcedure,
+            parameters, commandType: CommandType.StoredProcedure);
+
+        return result;
+    }
+
+    public void StartTransaction(string connectionStringName = DefaultDatabase)
+    {
+        string connectionString = GetConnectionString(connectionStringName);
+
+        _connection = new SqlConnection(connectionString);
+        _connection.Open();
+
+        _transaction = _connection.BeginTransaction();
+    }
+
+    public void CommitTransaction()
+    {
+        _transaction?.Commit();
+        _connection?.Close();
+
+        Dispose();
+    }
+
+    public void RollbackTransaction()
+    {
+        _transaction?.Rollback();
+        _connection?.Close();
+
+        Dispose();
+    }
+
+    public async Task<List<T>> LoadAllDataInTransactionAsync<T>(
+        string storedProcedure,
+        DynamicParameters parameters = null)
+    {
+        var rows = await _connection?.QueryAsync<T>(storedProcedure, parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return rows.ToList();
+    }
+
+    public async Task<T> LoadDataInTransactionAsync<T>(
+        string storedProcedure,
+        DynamicParameters parameters = null)
+    {
+        var row = await _connection?.QueryFirstOrDefaultAsync(
+            storedProcedure,
+            parameters,
+            commandType: CommandType.StoredProcedure);
+
+        return row;
+    }
+}
